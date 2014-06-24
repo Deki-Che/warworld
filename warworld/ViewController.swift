ab1401fa4fb18320ad3b713eeaae7a21fb804cf3
ldci <ldci@ldcimatoMacBook-Pro-2.local>
2014-06-24 11:52:45 +0800

    Initial Commit

---
+++ warworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  warworld
+//
+//  Created by ldci on 14/6/24.
+//  Copyright (c) 2014年 郑赟. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ warworldTests/warworldTests.swift
@@ -0,0 +1,35 @@
+//
+//  warworldTests.swift
+//  warworldTests
+//
+//  Created by ldci on 14/6/24.
+//  Copyright (c) 2014年 郑赟. All rights reserved.
+//
+
+import XCTest
+
+class warworldTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
